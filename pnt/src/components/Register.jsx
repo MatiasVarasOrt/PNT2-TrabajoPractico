eb6646ee1856567b743267e0286cb49912341933
MatiasVarasOrt <35426840@comunidadort.edu.ar>
2025-11-18 12:12:55 -0300

    eliminar debugger

--- pnt/src/components/Register.jsx
+++ pnt/src/components/Register.jsx
@@ -66,7 +66,7 @@ export default function Register() {
     if (!validateForm()) {
       return;
     }
-debugger
+    
     try {
       const result = await register({
         name: formData.name,
